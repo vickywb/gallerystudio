ba092731dd23bbc866455015e8f79499c6b04af0
vicky wibisono <wibivicky@gmail.com>
2024-08-29 16:46:23 +0700

    update auth controller

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -2,13 +2,14 @@
 
 namespace App\Http\Controllers\Auth;
 
-use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
-    public function login()
+    public function loginPage()
     {
         return view('auth.login');
     }
@@ -17,12 +18,40 @@ class AuthController extends Controller
     {
         $user = User::where('email', $request->email)->first();
 
+        $credentials = $request->validate([
+            'email' => ['required', 'email'],
+            'password' => ['required'],
+        ]);
+ 
+        if (Auth::attempt($credentials)) {
+            $request->session()->regenerate();
+ 
+            return redirect()->intended(route('admin.dashboard'));
+        }
+ 
+        return back()->withErrors([
+            'email' => 'The provided credentials do not match our records.',
+        ])->onlyInput('email');
+
         if (!$user) {
            return redirect()->back()->withErrors([
             'errors' => "Email or password doesn't match."
            ]);
         }
 
-        return "login success";
+        Auth::login($user, true);
+
+        return redirect()->route('admin.dashboard');
+    }
+
+    public function logout(Request $request)
+    {
+        Auth::logout();
+ 
+        $request->session()->invalidate();
+     
+        $request->session()->regenerateToken();
+     
+        return redirect()->route('home');
     }
 }
