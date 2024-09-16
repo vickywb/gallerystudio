544b8f08685fe0826bb3ea917e8cbba42261626f
vicky wibisono <wibivicky@gmail.com>
2024-09-16 09:27:56 +0700

    update login ui

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,21 +5,25 @@
 
 <div class="login-body">
     <div class="card-body" style="max-width: 400px">
+
+        @error('errors')
+            <div class="alert alert-danger">{{ $message }}</div>
+        @enderror
+        
+        @error('throttle')
+            <div class="alert alert-danger">{{ $message }}</div>
+        @enderror
+
         <div class="sign-in-form">
+
             <h2>Admin Login</h2>
 
             <form action="{{ route('loginProccess') }}" method="post">
                 @csrf
                 
                 <input type="email" placeholder="Email" name="email" id="email" required>
-                @error('email')
-                    <div class="alert alert-danger">{{ $message }}</div>
-                @enderror
 
                 <input type="password" placeholder="Password" name="password" id="password" required>
-                @error('Password')
-                    <div class="alert alert-danger">{{ $message }}</div>
-                @enderror
 
                 <button type="submit">Sign In</button>
             </form>
