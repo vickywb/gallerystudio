a446ea490dc6e20d84fd619a9e6c8b68f0905277
vicky wibisono <wibivicky@gmail.com>
2024-09-16 09:22:12 +0700

    update admin portofolio controller

--- app/Http/Controllers/Admin/PortofolioController.php
+++ app/Http/Controllers/Admin/PortofolioController.php
@@ -2,43 +2,237 @@
 
 namespace App\Http\Controllers\Admin;
 
-use App\Http\Controllers\Controller;
+use App\Models\Category;
+use App\Models\Portofolio;
+use App\Helpers\UploadFile;
+use Illuminate\Support\Str;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use App\Http\Controllers\Controller;
+use App\Repositories\FileRepository;
+use App\Repositories\PortofolioRepository;
+use App\Http\Requests\PortofolioStoreRequest;
+use App\Http\Requests\PortofolioUpdateRequest;
+use App\Models\File;
 
 class PortofolioController extends Controller
 {
+    private $portofolioRepository;
+    private $fileRepository;
+
+    public function __construct(
+        PortofolioRepository $portofolioRepository,
+        FileRepository $fileRepository
+    ) 
+    {
+        $this->portofolioRepository = $portofolioRepository;
+        $this->fileRepository = $fileRepository;
+    }
+    
     public function index()
     {
-        //
+        $portofolios = $this->portofolioRepository->get([
+            'order' => 'title asc'
+        ]);
+
+        return view('admin.portofolio.index', [
+            'portofolios' => $portofolios
+        ]);
     }
 
     public function create()
     {
-        //
+        $categories = Category::all();
+
+        return view('admin.portofolio.create', [
+            'categories' => $categories
+        ]);
     }
 
-    public function store(Request $request)
+    public function store(PortofolioStoreRequest $request, Portofolio $portofolio)
     {
-        //
+        //Create variable to store array data
+        $listImages = [];
+
+        $request->merge([
+            'slug' => Str::slug($request->title)
+        ]);
+
+        //Store request in variable data
+        $data = $request->only([
+            'title', 'slug', 'category_id', 'description'
+        ]);
+
+        try {
+            DB::beginTransaction();
+
+            $portofolio = new Portofolio($data);
+            $portofolio = $this->portofolioRepository->store($portofolio);
+
+            //Checking if a File is Uploaded
+            if ($request->hasFile('image')) {
+                //Retrieving the Uploaded File
+                $file = $request->file('image')->get();
+
+                //Helpers check and store
+                new UploadFile($file, [
+                    'field_name' => 'location',
+                    'extension' => $request->file('image')->getClientOriginalExtension(),
+                    'location' => 'portofolio/'
+                ], $request);
+
+                //Storing information to repository with the key "location"
+                $uploadedFile = $this->fileRepository->store($request->only('location'));
+
+                //Store data in array
+                $listImages[] = [
+                    'file_id' => $uploadedFile->id,
+                    'portofolio_id' => $portofolio->id
+                ];
+            }
+            
+            //Store data listImages to relation table
+            $portofolio->portofolioImages()->createMany($listImages);
+
+            DB::commit();
+        } catch (\Throwable $th) {
+            DB::rollBack();
+
+            return redirect()->back()->withErrors([
+                'errors' => $th->getMessage()
+            ]);
+        }
+
+        return to_route('admin.portofolio.index')->with([
+            'success' => 'New Portofolio has successfully created.'
+        ]);
     }
 
-    public function show(string $id)
+    public function edit(Portofolio $portofolio)
     {
-        //
+        $categories = Category::all();
+
+        return view('admin.portofolio.edit', [
+            'portofolio' => $portofolio,
+            'categories' => $categories
+        ]);
     }
 
-    public function edit(string $id)
+    public function update(PortofolioUpdateRequest $request, Portofolio $portofolio)
     {
-        //
+        //Create variable to store array data
+        $listImages = [];
+
+        //Get Unused File according id and location
+        $unusedFiles = File::select('id', 'location')
+            ->doesntHave('abouts')
+            ->doesntHave('portofolioImages')
+            ->doesntHave('blogs')
+            ->get();
+
+        $request->merge([
+            'slug' => Str::slug($request->title)
+        ]);
+
+        //Store request in variable data
+        $data = $request->only([
+            'title', 'slug', 'category_id', 'description'
+        ]);
+
+        try {
+            DB::beginTransaction();
+
+            //Fill request data 
+            $portofolio = $portofolio->fill($data);
+            $portofolio = $this->portofolioRepository->store($portofolio);
+
+            //Checking if a File is Uploaded
+            if ($request->hasFile('image')) {
+                //Retrieving the Uploaded File
+                $file = $request->file('image')->get();
+            
+                //Helpers check
+                new UploadFile($file, [
+                    'field_name' => 'location',
+                    'extension' => $request->file('image')->getClientOriginalExtension(),
+                    'location' => 'Portofolio/'
+                ], $request);
+
+                //Storing information to repository with the key "location"
+                $uploadedFile = $this->fileRepository->store($request->only('location'));
+
+                //Check file id
+                if ($portofolio->portofolioImages->file_id) {
+                    $oldFileName = $portofolio->portofolioImages->file->location;
+                }
+
+                //Check File is empty or not
+                if (!$unusedFiles->isEmpty()) {
+                    //File which not in relation will be execute 
+                    foreach ($unusedFiles as $file) {
+                        $file->delete();
+                    }
+                }
+
+                //Store data in array
+                $listImages[] = [
+                    'file_id' => $uploadedFile->id,
+                    'portofolio_id' => $portofolio->id
+                ];
+            }
+
+            //Store data listImages to relation table
+            $portofolio->portofolioImages()->createMany($listImages);
+
+            DB::commit();
+        } catch (\Throwable $th) {
+            DB::rollback();
+
+            return redirect()->back()->withErrors([
+                'errors' => $th->getMessage()
+            ]);
+        }
+
+        return to_route('admin.portofolio.index')->with([
+            'success' => 'Portofolio has successfully updated.'
+        ]);
     }
 
-    public function update(Request $request, string $id)
+    public function destroy(Portofolio $portofolio)
     {
-        //
-    }
+        try {
+            DB::beginTransaction();
 
-    public function destroy(string $id)
-    {
-        //
+            if (!empty($portofolio->portofolioImages->file_id)) {
+                
+                // Retrieve the location of the old file
+                if ($portofolio->portofolioImages->file_id) {
+                    $oldFileName = $portofolio->portofolioImages->file->location;
+                }
+
+                //Delete the existing file in the storage
+                if (isset($oldFileName)) {
+                    Storage::delete($oldFileName);
+                }
+
+                // Delete the file record from the specific table
+                $file = File::find($portofolio->portofolioImages->file->id)->delete();
+            }
+
+            //Delete a record from a specific table
+            $portofolio->delete();
+
+            DB::commit();
+        } catch (\Throwable $th) {
+            DB::rollback();
+
+            return redirect()->back()->withErrors([
+                'errors' => $th->getMessage()
+            ]);
+        }
+
+        return to_route('admin.portofolio.index')->with([
+            'success' => 'Portofolio has been deleted.'
+        ]);
     }
 }
