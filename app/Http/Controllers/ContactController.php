4d6bfe1d77d63dfb69390f2223b9632e9ca79a98
vicky wibisono <wibivicky@gmail.com>
2024-09-16 09:39:55 +0700

    update user contact controller

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -2,42 +2,51 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\ContactStoreRequest;
+use App\Models\Contact;
+use App\Repositories\ContactRepository;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class ContactController extends Controller
 {
+    private $contactRepository;
+
+    public function __construct(ContactRepository $contactRepository) 
+    {
+        $this->contactRepository = $contactRepository;
+    }
+
     public function index()
     {
         return view('frontend.contact');
     }
 
-    public function create()
+    public function store(ContactStoreRequest $request, Contact $contact)
     {
-        //
-    }
+        //Store data in variable data
+        $data = $request->only([
+            'name', 'email', 'subject', 'message'
+        ]);
 
-    public function store(Request $request)
-    {
-        //
-    }
+        try {
+            DB::beginTransaction();
 
-    public function show(string $id)
-    {
-        //
-    }
+            //Store the new data
+            $contact = new Contact($data);
+            $contact = $this->contactRepository->store($contact);
 
-    public function edit(string $id)
-    {
-        //
-    }
+            DB::commit();
+        } catch (\Throwable $th) {
+            DB::rollBack();
 
-    public function update(Request $request, string $id)
-    {
-        //
-    }
+            return redirect()->back()->withErrors([
+                'errors' => $th->getMessage()
+            ]);
+        }
 
-    public function destroy(string $id)
-    {
-        //
+        return to_route('contact')->with([
+            'success' => 'Your Message is successfully send, please be patient we will reply your message ASAP! Thankyou.'
+        ]);
     }
 }
