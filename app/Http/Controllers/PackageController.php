827b883a49f400fd7515cc19c8a3afc0c50c9edb
vicky wibisono <wibivicky@gmail.com>
2024-09-23 00:34:02 +0700

    update user package controller

--- app/Http/Controllers/PackageController.php
+++ app/Http/Controllers/PackageController.php
@@ -2,42 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Package;
 use Illuminate\Http\Request;
 
 class PackageController extends Controller
 {
     public function index()
     {
-        return view('frontend.package');
-    }
-
-    public function create()
-    {
-        //
-    }
-
-    public function store(Request $request)
-    {
-        //
-    }
-
-    public function show(string $id)
-    {
-        //
-    }
-
-    public function edit(string $id)
-    {
-        //
-    }
-
-    public function update(Request $request, string $id)
-    {
-        //
-    }
-
-    public function destroy(string $id)
-    {
-        //
+        $packages = Package::all();
+        return view('frontend.package',[
+            'packages' => $packages
+        ]);
     }
 }
