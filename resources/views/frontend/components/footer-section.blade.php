77f62b17b1221b75fb8aedd26c0e4d3974a699db
vicky wibisono <wibivicky@gmail.com>
2024-09-25 00:38:19 +0700

    add footer section for user frontend

---
+++ resources/views/frontend/components/footer-section.blade.php
@@ -0,0 +1,27 @@
+  <!-- Footer Area Start -->
+  <footer class="footer-area">
+    <div class="container-fluid h-100">
+        <div class="row h-100">
+            <div class="col-12 h-100">
+                <div class="footer-content h-100 d-md-flex align-items-center justify-content-center">
+                    <!-- Single Footer Content -->
+                    <div class="single-footer-content">
+                        <img src="{{ asset('frontend/img/core-img/map.png') }}" alt="">
+                        <a href="#">Jalan Jeruk no. 99, JAKARTA</a>
+                    </div>
+                    <!-- Single Footer Content -->
+                    <div class="single-footer-content">
+                        <img src="{{ asset('frontend/img/core-img/smartphone.png') }}" alt="">
+                        <a href="#">+628 111 111 111</a>
+                    </div>
+                    <!-- Single Footer Content -->
+                    <div class="single-footer-content">
+                        <img src="{{ asset('frontend/img/core-img/envelope-2.png') }}" alt="">
+                        <a href="#">hello@company.com</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
+<!-- Footer Area End -->
