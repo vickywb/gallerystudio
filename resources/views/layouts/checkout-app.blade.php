69362192a7800a8f5f345347150d2b6c64b38bf9
vicky wibisono <wibivicky@gmail.com>
2024-10-08 09:09:15 +0700

    add new layout for checkout

---
+++ resources/views/layouts/checkout-app.blade.php
@@ -0,0 +1,25 @@
+<!-- resources/views/layouts/app.blade.php -->
+<!DOCTYPE html>
+<html lang="en">
+   <head>
+       <meta charset="UTF-8">
+       <meta name="description" content="Studio Gallery">
+       <meta http-equiv="X-UA-Compatible" content="IE=edge">
+       <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+       <!-- The above 4 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+   
+       <!-- Title  -->
+       <title>@yield('title')</title>
+   
+       <!-- Core Style CSS -->
+       <link rel="stylesheet" href="{{ asset('frontend/css/core-style.css') }}">
+   </head>
+   <body>
+
+       <div class="container">
+           @yield('content')
+       </div>
+       
+   </body>
+</html>
+
