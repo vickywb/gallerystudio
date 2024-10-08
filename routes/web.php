24dd5c187b8389e87cc48298ffb23a3d93c95896
vicky wibisono <wibivicky@gmail.com>
2024-10-08 09:10:02 +0700

    update web routes

--- routes/web.php
+++ routes/web.php
@@ -3,13 +3,15 @@
 use App\Http\Controllers\AboutController;
 use App\Http\Controllers\Admin\AboutController as AdminAboutController;
 use App\Http\Controllers\Admin\BlogController as AdminBlogController;
-use App\Http\Controllers\Admin\CategoryController;
+use App\Http\Controllers\Admin\CategoryController as AdminCategoryController;
 use App\Http\Controllers\Admin\ContactController as AdminContactController;
 use App\Http\Controllers\Admin\DashboardController;
 use App\Http\Controllers\Admin\PackageController as AdminPackageController;
 use App\Http\Controllers\Admin\PortofolioController as AdminPortofolioController;
+use App\Http\Controllers\Admin\UserController as AdminUserController;
 use App\Http\Controllers\Auth\AuthController;
 use App\Http\Controllers\BlogController;
+use App\Http\Controllers\Checkout\TransactionController;
 use App\Http\Controllers\ContactController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\PackageController;
@@ -25,6 +27,9 @@ Route::get('/packages', [PackageController::class, 'index'])->name('package');
 Route::get('/contacts', [ContactController::class, 'index'])->name('contact');
 Route::post('/contact/store', [ContactController::class, 'store'])->name('contact.store');
 Route::get('/captcha-refresh', [ContactController::class, 'captchaRefresh'])->name('captcha.refresh');
+
+//Checkout Package
+Route::get('/packages/{package:slug}/checkouts', [TransactionController::class, 'checkoutPage'])->name('checkout.package');
 
 //Login Admin Route
 Route::controller(AuthController::class)
@@ -51,58 +56,67 @@ Route::prefix('admin')
         });
         
         //Category
-        Route::controller(CategoryController::class)->group(function () {
-            Route::get('/category', 'index')->name('admin.category.index');
-            Route::get('/category/create', 'create')->name('admin.category.create');
-            Route::post('/category/store', 'store')->name('admin.category.store');
-            Route::get('/category/{category}/edit', 'edit')->name('admin.category.edit');
-            Route::patch('/category/{category}/update', 'update')->name('admin.category.update');
-            Route::delete('/category/{category}/delete', 'destroy')->name('admin.category.delete');
+        Route::controller(AdminCategoryController::class)->group(function () {
+            Route::get('/categories', 'index')->name('admin.category.index');
+            Route::get('/categories/create', 'create')->name('admin.category.create');
+            Route::post('/categories/store', 'store')->name('admin.category.store');
+            Route::get('/categories/{category}/edit', 'edit')->name('admin.category.edit');
+            Route::patch('/categories/{category}/update', 'update')->name('admin.category.update');
+            Route::delete('/categories/{category}/delete', 'destroy')->name('admin.category.delete');
         });
 
         //About
         Route::controller(AdminAboutController::class)->group(function () {
-            Route::get('/about', 'index')->name('admin.about.index');
-            Route::get('/about/create', 'create')->name('admin.about.create');
-            Route::post('/about/store', 'store')->name('admin.about.store');
-            Route::get('/about/{about}/edit', 'edit')->name('admin.about.edit');
-            Route::patch('/about/{about}/update', 'update')->name('admin.about.update');
-            Route::delete('/about/{about}/delete', 'destroy')->name('admin.about.delete');
+            Route::get('/abouts', 'index')->name('admin.about.index');
+            Route::get('/abouts/create', 'create')->name('admin.about.create');
+            Route::post('/abouts/store', 'store')->name('admin.about.store');
+            Route::get('/abouts/{about}/edit', 'edit')->name('admin.about.edit');
+            Route::patch('/abouts/{about}/update', 'update')->name('admin.about.update');
+            Route::delete('/abouts/{about}/delete', 'destroy')->name('admin.about.delete');
         });
 
         //Portofolio
         Route::controller(AdminPortofolioController::class)->group(function () {
-            Route::get('/portofolio', 'index')->name('admin.portofolio.index');
-            Route::get('/portofolio/create', 'create')->name('admin.portofolio.create');
-            Route::post('/portofolio/store', 'store')->name('admin.portofolio.store');
-            Route::get('/portofolio/{portofolio}/edit', 'edit')->name('admin.portofolio.edit');
-            Route::patch('/portofolio/{portofolio}/update', 'update')->name('admin.portofolio.update');
-            Route::delete('/portofolio/{portofolio}/delete', 'destroy')->name('admin.portofolio.delete');
+            Route::get('/portofolios', 'index')->name('admin.portofolio.index');
+            Route::get('/portofolios/create', 'create')->name('admin.portofolio.create');
+            Route::post('/portofolios/store', 'store')->name('admin.portofolio.store');
+            Route::get('/portofolios/{portofolio}/edit', 'edit')->name('admin.portofolio.edit');
+            Route::patch('/portofolios/{portofolio}/update', 'update')->name('admin.portofolio.update');
+            Route::delete('/portofolios/{portofolio}/delete', 'destroy')->name('admin.portofolio.delete');
         });
                 
         //Blog
         Route::controller(AdminBlogController::class)->group(function () {
-            Route::get('/blog', 'index')->name('admin.blog.index');
-            Route::get('/blog/create', 'create')->name('admin.blog.create');
-            Route::post('/blog/store', 'store')->name('admin.blog.store');
-            Route::get('/blog/{blog}/edit', 'edit')->name('admin.blog.edit');
-            Route::patch('/blog/{blog}/update', 'update')->name('admin.blog.update');
-            Route::delete('/blog/{blog}/delete', 'destroy')->name('admin.blog.delete');
+            Route::get('/blogs', 'index')->name('admin.blog.index');
+            Route::get('/blogs/create', 'create')->name('admin.blog.create');
+            Route::post('/blogs/store', 'store')->name('admin.blog.store');
+            Route::get('/blogs/{blog}/edit', 'edit')->name('admin.blog.edit');
+            Route::patch('/blogs/{blog}/update', 'update')->name('admin.blog.update');
+            Route::delete('/blogs/{blog}/delete', 'destroy')->name('admin.blog.delete');
         });
                 
         //Package
         Route::controller(AdminPackageController::class)->group(function () {
-            Route::get('/package', 'index')->name('admin.package.index');
-            Route::get('/package/create', 'create')->name('admin.package.create');
-            Route::post('/package/store', 'store')->name('admin.package.store');
-            Route::get('/package/{package}/edit', 'edit')->name('admin.package.edit');
-            Route::patch('/package/{package}/update', 'update')->name('admin.package.update');
-            Route::delete('/package/{package}/delete', 'destroy')->name('admin.package.delete');
+            Route::get('/packages', 'index')->name('admin.package.index');
+            Route::get('/packages/create', 'create')->name('admin.package.create');
+            Route::post('/packages/store', 'store')->name('admin.package.store');
+            Route::get('/packages/{package}/edit', 'edit')->name('admin.package.edit');
+            Route::patch('/packages/{package}/update', 'update')->name('admin.package.update');
+            Route::delete('/packages/{package}/delete', 'destroy')->name('admin.package.delete');
+
+            //Package Detail
+            Route::get('/packages/{package}/detail', 'detail')->name('admin.package.detail');
         });
 
         //Contact
         Route::controller(AdminContactController::class)->group(function () {
-            Route::get('/contact', 'index')->name('admin.contact.index');
-            Route::delete('/contact/delete/{contact}', 'destroy')->name('admin.contact.delete');
+            Route::get('/contacts', 'index')->name('admin.contact.index');
+            Route::delete('/contacts/delete/{contact}', 'destroy')->name('admin.contact.delete');
+        });
+
+        //User
+        Route::controller(AdminUserController::class)->group(function () {
+            Route::get('/users', 'index')->name('admin.user.index');
+            Route::get('/users/create', 'create')->name('admin.user.create');
         });
 });
