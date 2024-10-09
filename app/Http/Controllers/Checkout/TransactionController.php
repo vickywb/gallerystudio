0b9f68602a0348905a34f917d5bd14549ddca287
vicky wibisono <wibivicky@gmail.com>
2024-10-09 12:27:18 +0700

    update user transaction controller

--- app/Http/Controllers/Checkout/TransactionController.php
+++ app/Http/Controllers/Checkout/TransactionController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Checkout;
 
 use App\Http\Controllers\Controller;
 use App\Models\Package;
+use App\Models\Transaction;
 use Illuminate\Http\Request;
 use Xendit\Configuration;
 use Xendit\Invoice\InvoiceApi;
@@ -13,15 +14,22 @@ class TransactionController extends Controller
 {
     private $keyApi;
     
-    public function checkoutPage(Package $package)
+    public function checkoutPage(Package $package, Transaction $transaction)
     {
+        $paymentChannels = Transaction::PAYMENT_METHODS_MAP;
+
         return view('frontend.checkouts.checkout', [
-            'package' => $package
+            'package' => $package,
+            'paymentChannels' => $paymentChannels,
+            'transaction' => $transaction
         ]);
     }
 
-    public function checkout()
+    public function checkout(Request $request, Package $package)
     {
-        
+        $invoiceId = 'Order' . '-' . time();
+        dd($invoiceId);
+        dd($package->id);
+        dd($request->all());
     }
 }
