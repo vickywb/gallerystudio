9e358a9bcd131f82c64bcc1541e2cdc686492838
vicky wibisono <wibivicky@gmail.com>
2024-10-09 12:24:46 +0700

    update on admin transaction index

---
+++ app/Http/Controllers/Admin/TransactionController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\Transaction;
+use Illuminate\Http\Request;
+
+class TransactionController extends Controller
+{
+    public function index()
+    {
+        $transactions = Transaction::all();
+
+        return view('admin.transactions.index', [
+            'transactions' => $transactions
+        ]);
+    }
+
+    public function create()
+    {
+        //
+    }
+
+    public function store(Request $request)
+    {
+        //
+    }
+
+    public function show(string $id)
+    {
+        //
+    }
+
+    public function edit(string $id)
+    {
+        //
+    }
+
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    public function destroy(string $id)
+    {
+        //
+    }
+}
