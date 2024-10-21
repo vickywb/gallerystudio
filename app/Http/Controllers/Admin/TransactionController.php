07b8905528e4cf1edf1acd5603beef25b84442e1
vicky wibisono <wibivicky@gmail.com>
2024-10-22 00:49:13 +0700

    update admin transaction controller

--- app/Http/Controllers/Admin/TransactionController.php
+++ app/Http/Controllers/Admin/TransactionController.php
@@ -5,12 +5,13 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use App\Models\Transaction;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class TransactionController extends Controller
 {
     public function index()
     {
-        $transactions = Transaction::all();
+        $transactions = Transaction::paginate(5);
 
         return view('admin.transactions.index', [
             'transactions' => $transactions
@@ -42,8 +43,24 @@ class TransactionController extends Controller
         //
     }
 
-    public function destroy(string $id)
+    public function destroy(Transaction $transaction)
     {
-        //
+        try {
+            DB::beginTransaction();
+            
+            $transaction->delete();
+
+            DB::commit();
+        } catch (\Throwable $th) {
+            DB::rollBack();
+
+            return redirect()->back()->withErrors([
+                'errors' => $th->getMessage()
+            ]);
+        }
+
+        return to_route('admin.transaction.index')->with([
+            'success' => 'Transaction has been successfully deleted.'
+        ]);
     }
 }
