54da167e8d3a9339608652226017e212f0b7a26d
vicky wibisono <wibivicky@gmail.com>
2024-10-22 00:43:15 +0700

    update frontend user blog views

--- resources/views/frontend/blog-detail.blade.php
+++ resources/views/frontend/blog-detail.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.app')
 
-@section('title', 'Blog')
+@section('title', 'Blog ' . $blog->title .' Detail')
 @section('content')
         <!-- Blog Area Start -->
         <section class="blog-area section_padding_100 mt-100">
@@ -10,33 +10,22 @@
                     <!-- Single Blog Area -->
                     <div class="col-10">
                         <div class="single-blog-area text-center mb-100 wow fadeInUpBig" data-wow-delay="100ms" data-wow-duration="1s">
-                            @foreach ($blogs as $blog)
                                 @foreach ($blog->blogImages as $blogImage)
                                 <div class="blog-thumbnail mb-50">
                                     <img src="{{ $blogImage->showFile ?? asset('frontend/img/no-image.png') }}" alt="blog-image">
                                 </div>
                                 <div class="blog-content">
                                     <h2>{{ $blog->title }}</h2>
-                                    <a href="#" class="post-date">Updated {{ $blog->created_at->diffForHumans() }}</a>
-                                    <a href="#" class="post-author">By Admin</a>
-                                    <p>{!! Str::words($blog->description, 50, '...') !!}</p>
-                                    <a href="{{ route('blog.detail', $blog->slug) }}" class="btn studio-btn mb-5"><img src="img/core-img/logo-icon.png" alt="">Read More</a>
+                                    <a class="post-date">Updated {{ $blog->created_at->diffForHumans() }}</a>
+                                    <a class="post-author">By Admin</a>
+                                    <p>{!! Str::words($blog->description) !!}</p>
+                                    <a href="{{ route('blog') }}" class="btn studio-btn mb-5"><img src="img/core-img/logo-icon.png" alt="">Back</a>
                                 </div>
                                 @endforeach
-                            @endforeach
                         </div>
                     </div>
                 </div>
-                <!-- Pagination -->
-                <div class="row">
-                    <div class="col-12">
-                        <nav aria-label="Page navigation" class="pagination-area mb-100">
-                            <ul class="pagination justify-content-center">
-                                {{ $blogs->links() }}
-                            </ul>
-                        </nav>
-                    </div>
-                </div>
+               
             </div>
         </section>
         <!-- Blog Area End -->
@@ -45,3 +34,4 @@
         @include('frontend.components.footer-section')
         <!-- Footer Area End -->
 @endsection
+
